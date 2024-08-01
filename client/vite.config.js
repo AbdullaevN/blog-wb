985480759fb7de9147f327f83e5aee6aabd509c1
AbdullaevN <nurlanabdullaev9820@gmail.com>
2024-08-01 20:57:52 +0600

    client build

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
+    historyApiFallback: true,
     proxy: {
       '/api': 'http://localhost:8800',
     },
   },
+
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+ 
 })
